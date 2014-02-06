dcc18e81998b1eff7cac016a6e696c036f523f3e
jjesus <john.j.jesus@gmail.com>
2014-02-06 16:55:22 -0600

    Add second connector. Navigate to name of components.

--- JohnJesus.DeviceXml/SystemTopoXmlReader.cs
+++ JohnJesus.DeviceXml/SystemTopoXmlReader.cs
@@ -30,31 +30,29 @@ namespace JohnJesus.DeviceXml
         }
         public void CreateObjects()
         {
-            var racks = XDoc.Descendants("rack");
-            foreach (var r in racks)
-            {
-                Console.WriteLine("{0}", r.Element("name").Value);
-            }
-
             var connectors = XDoc.Descendants("connector");
-            var conn_sources = XDoc.Descendants("connector_src");
-            var conn_dests = XDoc.Descendants("connector_dst");
-            foreach (var c in connectors)
+            var conns = from _src in XDoc.Descendants("connector_src")
+                        join
+                        _dst in XDoc.Descendants("connector_dst")
+                        on _src.Element("connector_id").Value equals _dst.Element("connector_id").Value
+                        select new
+                        {
+                            //Name = c.Element("name").Value,
+                            Source = _src.Element("card_id").Value,
+                            Dest = _dst.Element("card_id").Value
+                        };
+            foreach (var conn in conns)
             {
-                string id = c.Element("id").Value;
-                var conns = from _src in conn_sources
-                            join
-                            _dst in conn_dests
-                            on _src.Element("connector_id").Value equals _dst.Element("connector_id").Value
-                            select new
-                            {
-                                Source = _src.Element("card_id").Value,
-                                Dest = _dst.Element("card_id").Value
-                            };
-                foreach (var conn in conns)
-                {
-                    Console.WriteLine("Src:{0} Dst:{1}", conn.Source, conn.Dest);
-                }
+                var src = (from _card in XDoc.Descendants("card")
+                            where _card.Element("id").Value == conn.Source
+                            select _card)
+                          .First();
+                var dst = (from _card in XDoc.Descendants("card")
+                           where _card.Element("id").Value == conn.Dest
+                           select _card)
+                          .First();
+                Console.WriteLine("Src:{0} Dst:{1}", 
+                    src.Element("name").Value, dst.Element("name").Value);
             }
         }
     }
