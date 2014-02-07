2489d622252e42ae547f917c620f6fde7925a8f0
jjesus <john.j.jesus@gmail.com>
2014-02-07 16:49:47 -0600

    Many new files for MVVM.
    Initial tree view displays only Racks using the
    RackViewModel.  Model is hard-coded for now.

---
+++ JohnJesus.DeviceViewModel/SystemViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using JohnJesus.DeviceModel;
+
+namespace JohnJesus.DeviceViewModel
+{
+    public class SystemViewModel
+    {
+        List<Rack> _rackList;
+        public List<Rack> RackList
+        {
+            get { return _rackList; }
+            set { _rackList = value; }
+        }
+        public List<RackViewModel> RackViewModelList { get; set; }
+
+        public SystemViewModel()
+        {
+            RackList = new List<Rack>()
+            {
+                new Rack("RACK-1"),
+                new Rack("RACK-2"),
+            };
+            RackViewModelList = new List<RackViewModel>();
+
+            foreach (Rack rack in _rackList)
+            {
+                RackViewModelList.Add(new RackViewModel(rack));
+            }
+        }
+    }
+}
